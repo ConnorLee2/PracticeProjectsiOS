1a709410a3d553a00145cc8a5091ba1e596225c6
Connor Lee <22938115+ConnorLee2@users.noreply.github.com>
2020-07-01 21:33:35 +0100

    Project 5

---
+++ Project5WordGame/Project5WordGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project5WordGame
+//
+//  Created by Connor Lee on 01/07/2020.
+//  Copyright © 2020 Connor Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
