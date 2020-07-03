ed488c02583e031f82c7a4e04b58b9e80f80b224
Connor Lee <22938115+ConnorLee2@users.noreply.github.com>
2020-07-03 20:14:52 +0100

    Project 6.5

---
+++ Project6.5ShoppingList/Project6.5ShoppingList/ViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ViewController.swift
+//  Project6.5ShoppingList
+//
+//  Created by Connor Lee on 03/07/2020.
+//  Copyright © 2020 Connor Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var shoppingList = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Shopping list"
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addItem))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Clear", style: .plain, target: self, action: #selector(clearList))
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return shoppingList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Item", for: indexPath)
+        cell.textLabel?.text = shoppingList[indexPath.row]
+        return cell
+    }
+    
+    @objc func addItem() {
+        let ac = UIAlertController(title: "Enter item", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        let submitAction = UIAlertAction(title: "Submit", style: .default) {
+            [weak self, weak ac] _ in
+            guard let item = ac?.textFields?[0].text else { return }
+            self?.submit(item)
+        }
+        
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+    }
+    
+    @objc func clearList() {
+        shoppingList.removeAll()
+        tableView.reloadData()
+    }
+    
+    func submit(_ item: String) {
+        shoppingList.insert(item, at: 0)
+        let indexPath = IndexPath(row: 0, section: 0)
+        tableView.insertRows(at: [indexPath], with: .automatic)
+    }
+}
+
