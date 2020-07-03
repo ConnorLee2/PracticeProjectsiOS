68d852a92bb2b8b7c8586ed4714057b7aa62af8f
Connor Lee <22938115+ConnorLee2@users.noreply.github.com>
2020-07-04 00:57:45 +0100

    Project 7

---
+++ Project7Json/Project7Json/Petitions.swift
@@ -0,0 +1,13 @@
+//
+//  Petitions.swift
+//  Project7Json
+//
+//  Created by Connor Lee on 03/07/2020.
+//  Copyright © 2020 Connor Lee. All rights reserved.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
