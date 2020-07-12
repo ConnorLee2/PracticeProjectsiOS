65ea40ab9887773b30fdc425ac0250e5aa326c7a
Connor Lee <22938115+ConnorLee2@users.noreply.github.com>
2020-07-13 00:43:30 +0100

    Project 12

---
+++ Project12/Project12UserDefaults/Project12UserDefaults/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Project12UserDefaults
+//
+//  Created by Connor Lee on 12/07/2020.
+//  Copyright © 2020 Connor Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        var defaults = UserDefaults.standard
+        
+        defaults.set(25, forKey: "Age")
+        defaults.set(true, forKey: "UseFaceID")
+        defaults.set(CGFloat.pi, forKey: "Pi")
+        defaults.set("Paul Hudson", forKey: "Name")
+        defaults.set(Date(), forKey: "LastRun")
+        
+        let array = ["Hello", "World"]
+        defaults.set(array, forKey: "SavedArray")
+        
+        let dict = ["Name": "Paul", "Country": "UK"]
+        defaults.set(dict, forKey: "SavedDictionary")
+        
+        let savedInteger = defaults.integer(forKey: "Age")
+        let savedBoolean = defaults.bool(forKey: "UseFaceID")
+        let savedCGFloat = defaults.float(forKey: "Pi")
+        
+        let savedArray = defaults.object(forKey: "SavedArray") as? [String] ?? [String]()
+        let savedDictionary = defaults.object(forKey: "SavedDictionary") as? [String: String] ?? [String: String]()
+        
+        
+    }
+
+
+}
+
---
+++ Project12/Project1StormViewer/Project1StormViewer/ViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ViewController.swift
+//  Project1StormViewer
+//
+//  Created by Connor Lee on 24/06/2020.
+//  Copyright © 2020 Connor Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var pictures = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Storm Viewer"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+        pictures.sort()
+        print(pictures)
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            vc.selectedImage = pictures[indexPath.row]
+            vc.selectedTitle = "Picture \(indexPath.row + 1) of \(pictures.count)"
+            save(key: pictures[indexPath.row])
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+    
+    func save(key: String) {
+        print(key)
+        let defaults = UserDefaults.standard
+        
+        // Get count and increase by one
+        var savedCount = defaults.integer(forKey: key)
+        print("before: \(savedCount)")
+        savedCount += 1
+        print("after: \(savedCount)")
+        
+        defaults.set(savedCount, forKey: key)
+    }
+}
+
