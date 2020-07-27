bf74b2563b8e1a098015c7e70de16f073f96e46a
Connor Lee <22938115+ConnorLee2@users.noreply.github.com>
2020-07-27 18:25:30 +0100

    Project 24

---
+++ Project24Strings.playground/Contents.swift
@@ -0,0 +1,122 @@
+import UIKit
+
+let name = "Taylor"
+
+for letter in name {
+    print("Give me a \(letter)")
+}
+
+let letter = name[name.index(name.startIndex, offsetBy: 3)]
+
+extension String {
+    subscript(i: Int) -> String {
+        return String(self[index(startIndex, offsetBy: i)])
+    }
+}
+
+let letter2 = name[3]
+
+let password = "12345"
+
+password.hasPrefix("123")
+password.hasSuffix("456")
+
+extension String {
+    func deletingPrefix(_ prefix: String) -> String {
+        guard self.hasPrefix(prefix) else { return self }
+        return String(self.dropFirst(prefix.count))
+    }
+    
+    func deletingSuffix(_ suffix: String) -> String {
+        guard self.hasSuffix(suffix) else { return self }
+        return String(self.dropLast(suffix.count))
+    }
+}
+
+let weather = "it's going to rain"
+print(weather.capitalized)
+
+extension String {
+    var capitalizedFirst: String {
+        guard let firstLetter = self.first else { return "" }
+        return firstLetter.uppercased() + self.dropFirst()
+    }
+}
+
+print(weather.capitalizedFirst)
+
+let input = "Swift is like Objective-C without the C"
+input.contains("Swift")
+
+let languages = ["Python", "Ruby", "Swift"]
+languages.contains("Swift")
+
+extension String {
+    func containsAny(of array: [String]) -> Bool {
+        for item in array {
+            if self.contains(item) {
+                return true
+            }
+        }
+        
+        return false
+    }
+}
+
+input.containsAny(of: languages)
+
+languages.contains(where: input.contains)
+
+let string = "This is a test string"
+let attributes: [NSAttributedString.Key: Any] = [
+    .foregroundColor: UIColor.white,
+    .backgroundColor: UIColor.red,
+    .font: UIFont.boldSystemFont(ofSize: 36)
+]
+
+let attributedString1 = NSAttributedString(string: string, attributes: attributes)
+
+let attributedString = NSMutableAttributedString(string: string)
+attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 8), range: NSRange(location: 0, length: 4))
+attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 16), range: NSRange(location: 5, length: 2))
+attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 24), range: NSRange(location: 8, length: 1))
+attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 32), range: NSRange(location: 10, length: 4))
+attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 40), range: NSRange(location: 15, length: 6))
+
+extension String {
+    func withPrefix(_ prefix: String) -> String {
+        guard self.hasPrefix(prefix) else { return  prefix + self }
+        
+        return self
+    }
+    var isNumeric: Bool {
+        guard let firstLetter = self.first else { return false }
+        // Create a String extension that adds an isNumeric property that returns true if the string holds any sort of number.
+        // Tip: creating a Double from a String is a failable initializer.
+        guard let double = Double(self) else { return false }
+        return true
+    }
+    
+    func lines() -> [String] {
+        // Create a String extension that adds a lines property that returns an array of all the lines in a string. So, “this\nis\na\ntest” should return an array with four elements.
+        return self.components(separatedBy: "\n")
+    }
+}
+
+let pet = "pet"
+print(pet.withPrefix("car"))
+
+let numeric = "1.0"
+print(numeric.isNumeric)
+
+let lines = "This\nis\na\ntest"
+print(lines.lines())
+
+let lines2 = """
+Monkey
+see
+monkey
+do
+"""
+
+print(lines2.lines())
